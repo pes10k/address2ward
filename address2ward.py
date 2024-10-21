35f3f4a8a94334199041adecd35a527759603ae2
Peter Snyder <snyderp@gmail.com>
2024-10-21 10:54:00 -0500

    init commit

---
+++ address2ward.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import argparse
+import pathlib
+import sys
+
+import address2ward
+
+PARSER = argparse.ArgumentParser(
+    prog="address2ward",
+    description="Returns best effort Chicago Aldermanic Ward for an address. "
+                "Ward data is from https://data.cityofchicago.org/Facilities-Geographic-Boundaries/Boundaries-Wards-2023-Map/cdf7-bgn3.")
+PARSER.add_argument(
+    "address",
+    help="Address to geolocate")
+PARSER.add_argument(
+    "--cachedir",
+    default="/tmp",
+    type=pathlib.Path,
+    help="If provided, path to read / write cached values to.")
+ARGS = PARSER.parse_args()
+
+WARD_ID = address2ward.ward_for_address(ARGS.address, ARGS.cachedir)
+if WARD_ID:
+    print(WARD_ID)
+    sys.exit(0)
+
+print(f"Unable to find ward for {ARGS.address}", file=sys.stderr)
+sys.exit(1)
---
+++ address2ward/__init__.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+import json
+from pathlib import Path
+from typing import cast
+
+from diskcache import Cache
+from geopy.geocoders import Nominatim
+import shapely
+import shapely.wkt
+
+
+@dataclass
+class Coords:
+    lat: float
+    long: float
+
+
+AddressCache = dict[str, tuple[float, float]]
+WardId = int
+
+
+@dataclass
+class WardShape:
+    ward: WardId
+    shape: shapely.MultiPolygon
+
+
+def get_ward_data() -> list[WardShape]:
+    wards = []
+    with open("./data/wards.json", "r") as json_data:
+        for ward_number, ward_shape in json.load(json_data):
+            ward = WardShape(int(ward_number), shapely.wkt.loads(ward_shape))
+            wards.append(ward)
+    return wards
+
+
+def coords_for_address(address: str, cachedir: Path | None = None) -> Coords:
+    cache: AddressCache | None = None
+    if cachedir:
+        cache = cast(AddressCache, Cache(cachedir))
+        if address in cache:
+            return Coords(*cache[address])
+
+    geolocator = Nominatim(user_agent="address2ward: map chicago addresses to chicago wards")
+    location = geolocator.geocode(address, timeout=30)
+    coords = Coords(location.latitude, location.longitude)
+
+    if cache is not None:
+        cache[address] = (coords.lat, coords.long)
+        cache.close()
+    return coords
+
+
+def ward_for_address(address: str,
+                     cachedir: Path | None = None) -> WardId | None:
+    coords = coords_for_address(address, cachedir)
+    point = shapely.Point(coords.long, coords.lat)
+    ward_data = get_ward_data()
+    for ward in ward_data:
+        if ward.shape.contains(point):
+            return ward.ward
+    return None
